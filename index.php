9ac3f277895a0a84304b3ed3bbd7be0089c9800d
Paulo Cesar <paulocesarsarrin@gmail.com>
2018-10-13 11:43:18 -0500

    con fe xD

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+	echo "algo se hace prro :v";
+?>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>pruebas</title>
+</head>
+<body>
+	Hola mundo :v
+</body>
+</html>
